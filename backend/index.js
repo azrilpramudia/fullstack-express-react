ed656c7d7ef6a9c4b43b3c6faef5cc258b3b2982
azrilpramudia <azrilpramudia01@gmail.com>
2025-07-06 03:50:19 +0700

    backend

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 3000;
+
+// use body parser
+app.use(bodyParser.urlencoded({extended: false}))
+
+// parse application/json
+app.use(bodyParser.json())
+
+// use cors
+app.use(cors())
+
+//route
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+// start server
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+})
