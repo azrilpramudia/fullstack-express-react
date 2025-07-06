88609d2e1edfa6e329a0515ae5b3953be19ea497
azrilpramudia <azrilpramudia01@gmail.com>
2025-07-07 02:57:11 +0700

    feat: add login

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const { Register } = require('../controllers/RegisterController');
+const loginController = require('../controllers/LoginController');
 const { validateRegister } = require('../utils/validators/auth');
+const { validateLogin } = require('../utils/validators/auth');
 
 router.post('/register', validateRegister, Register);
+router.post('/login', validateLogin, loginController.login);
 
 module.exports = router;
