6c766e4de3d4186cd7349633b6e08d4229c9f5e1
azrilpramudia <azrilpramudia01@gmail.com>
2025-08-07 00:45:39 +0700

    feat: add CRUD FE

--- frontend/src/views/admin/users/create.jsx
+++ frontend/src/views/admin/users/create.jsx
@@ -1,6 +1,37 @@
+import { useState } from "react";
 import SidebarMenu from "../../../components/SidebarMenu";
+import { useNavigate } from "react-router-dom";
+import Cookies from "js-cookie";
+import api from "../../../services/api";
+
+const token = Cookies.get("token");
 
 const UsersCreate = () => {
+  const navigate = useNavigate();
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [validation, setValidation] = useState([]);
+
+  const storeUser = async (e) => {
+    e.preventDefault();
+
+    api.defaults.headers.common["Authorization"] = token;
+    await api
+      .post("/api/admin/users", {
+        name: name,
+        email: email,
+        password: password,
+      })
+      .then(() => {
+        navigate("/admin/users");
+      })
+      .catch((error) => {
+        setValidation(error.response.data);
+      });
+  };
+
   return (
     <div className="container mt-5 mb-5">
       <div className="row">
@@ -10,7 +41,52 @@ const UsersCreate = () => {
         <div className="col-md-9">
           <div className="card border-0 rounded shadow-sm">
             <div className="card-header">Add User</div>
-            <div className="card-body">Halaman User Create</div>
+            <div className="card-body">
+              {validation.errors && (
+                <div className="alert alert-danger mt-2 pb-0">
+                  {validation.errors.map((error, index) => (
+                    <p key={index}>
+                      {error.path} : {error.msg}
+                    </p>
+                  ))}
+                </div>
+              )}
+              <form onSubmit={storeUser}>
+                <div className="form-group mb-3">
+                  <label className="mb-1 fw-bold">Full Name</label>
+                  <input
+                    type="text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    className="form-control"
+                    placeholder="Full Name"
+                  />
+                </div>
+                <div className="form-group mb-3">
+                  <label className="mb-1 fw-bold">Email Address</label>
+                  <input
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="form-control"
+                    placeholder="Email Address"
+                  />
+                </div>
+                <div className="form-group mb-3">
+                  <label className="mb-1 fw-bold">Password</label>
+                  <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    className="form-control"
+                    placeholder="Password"
+                  />
+                </div>
+                <button type="submit" className="btn btn-sm btn-primary">
+                  Save
+                </button>
+              </form>
+            </div>
           </div>
         </div>
       </div>
--- frontend/src/views/admin/users/index.jsx
+++ frontend/src/views/admin/users/index.jsx
@@ -1,7 +1,49 @@
+import { useState, useEffect } from "react";
 import SidebarMenu from "../../../components/SidebarMenu";
 import { Link } from "react-router-dom";
+import Cookies from "js-cookie";
+import api from "../../../services/api";
 
 const UsersIndex = () => {
+  const [users, setUsers] = useState([]);
+
+  const fetchDataUsers = async () => {
+    const token = Cookies.get("token");
+
+    if (token) {
+      api.defaults.headers.common["Authorization"] = token;
+      try {
+        const response = await api.get("/api/admin/users");
+        setUsers(response.data.data);
+      } catch (error) {
+        console.error("There was an error fetching the users!", error);
+      }
+    } else {
+      console.error("Token is not available!");
+    }
+  };
+
+  useEffect(() => {
+    fetchDataUsers();
+  }, []);
+
+  const deleteUser = async (id) => {
+    const token = Cookies.get("token");
+
+    if (token) {
+      api.defaults.headers.common["Authorization"] = token;
+
+      try {
+        await api.delete(`/api/admin/users/${id}`);
+        fetchDataUsers();
+      } catch (error) {
+        console.error("There was an error deleting the user!", error);
+      }
+    } else {
+      console.error("Token is not available!");
+    }
+  };
+
   return (
     <div className="container mt-5 mb-5">
       <div className="row">
@@ -16,10 +58,54 @@ const UsersIndex = () => {
                 to="/admin/users/create"
                 className="btn btn-sm btn-success rounded shadow-sm border-0"
               >
-                Add User
+                Add Users
               </Link>
             </div>
-            <div className="card-body">Halaman Users Index</div>
+            <div className="card-body">
+              <table className="table table-bordered">
+                <thead className="bg-dark text-white">
+                  <tr>
+                    <th scope="col">Full Name</th>
+                    <th scope="col">Email Address</th>
+                    <th scope="col" style={{ width: "17%" }}>
+                      Actions
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {users.length > 0 ? (
+                    users.map((user, index) => (
+                      <tr key={index}>
+                        <td>{user.name}</td>
+                        <td>{user.email}</td>
+                        <td className="text-center">
+                          <Link
+                            to={`/admin/users/edit/${user.id}`}
+                            className="btn btn-sm btn-primary rounded-sm shadow border-0 me-2"
+                          >
+                            Edit
+                          </Link>
+                          <button
+                            onClick={() => deleteUser(user.id)}
+                            className="btn btn-sm btn-danger rounded-sm shadow border-0"
+                          >
+                            Delete
+                          </button>
+                        </td>
+                      </tr>
+                    ))
+                  ) : (
+                    <tr>
+                      <td colSpan="4" className="text-center">
+                        <div className="alert alert-danger mb-0">
+                          Data Belum Tersedia!
+                        </div>
+                      </td>
+                    </tr>
+                  )}
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
       </div>
